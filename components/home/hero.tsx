6773ce6ec77cbafed61113a1027ddb5ab2b5a525
Oyelola Victoria <oyelolaabimbola23@gmail.com>
2026-06-30 00:24:19 +0100

    reverted hero text

--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -1,28 +1,28 @@
-import ExperienceForm from './experienceForm';
-import Image from 'next/image';
+import ExperienceForm from "./experienceForm";
+import Image from "next/image";
 
 export default function Hero() {
   return (
     <section
-      id='hero'
-      className='relative flex flex-col gap-10 items-center justify-center h-svh w-svw max-h-270 bg-[url(/hero_bg.webp)] bg-cover bg-center overflow-hidden px-5 lg:px-10'
+      id="hero"
+      className="relative flex flex-col gap-10 items-center justify-center h-svh w-svw max-h-270 bg-[url(/hero_bg.webp)] bg-cover bg-center overflow-hidden px-5 lg:px-10"
     >
       <div
         aria-hidden
-        className='z-2 pointer-events-none absolute w-full h-1/2 bottom-0 backdrop-blur-xl bg-primary mask-[linear-gradient(to_top,#cfbeea_20%,transparent)]'
+        className="z-2 pointer-events-none absolute w-full h-1/2 bottom-0 backdrop-blur-xl bg-primary mask-[linear-gradient(to_top,#cfbeea_20%,transparent)]"
       />
       <Image
-        src='/airplane_wing.webp'
+        src="/airplane_wing.webp"
         width={1068}
         height={832}
-        alt=''
-        className='absolute -bottom-15 sm:-bottom-50  md:-bottom-108 z-1'
+        alt=""
+        className="absolute -bottom-15 sm:-bottom-50  md:-bottom-108 z-1"
       />
-      <div className='max-w-400'>
-        <h1 className='relative z-3 text-dark-base text-hero text-center max-w-85 lg:max-w-175 opacity-80 mb-5'>
-          Explore the world like you belong there
+      <div className="max-w-400">
+        <h1 className="relative z-3 text-dark-base text-hero text-center max-w-85 lg:max-w-175 opacity-80 mb-5">
+          Travel like you belong there
         </h1>
-        <div className='relative z-10'>
+        <div className="relative z-10">
           <ExperienceForm />
         </div>
       </div>
